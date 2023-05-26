1d2796c51542e1385608736b5a555267debbd6df
Ibnu Farhan Shihab <farhanshihab.fs@gmail.com>
2023-05-26 21:00:25 +0700

    unit test porducts

--- model/products.go
+++ model/products.go
@@ -5,8 +5,8 @@ import "time"
 type Products struct {
 	ID          int64     `gorm:"primaryKey;column:id" validate:"required" json:"id"`
 	Name        string    `gorm:"column:name" validate:"required" json:"name"`
-	Amount      string    `gorm:"column:amount" validate:"required" json:"amount"`
-	Installment string    `gorm:"column:installment" validate:"required" json:"installment"`
-	Bunga       string    `gorm:"column:bunga" validate:"required" json:"bunga"`
+	Amount      float64   `gorm:"column:amount" validate:"required" json:"amount"`
+	Installment int64     `gorm:"column:installment" validate:"required" json:"installment"`
+	Bunga       float64   `gorm:"column:bunga" validate:"required" json:"bunga"`
 	Created_At  time.Time `gorm:"column:created_at" validate:"required" json:"created_at"`
 }
